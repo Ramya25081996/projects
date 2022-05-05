b70fb8dc8e68eb49fccb1c47d35a31a19d1fda96
Ramya25081996 <ramyad965@gmail.com>
2022-05-05 13:17:07 +0530

    adding medicalstore app

---
+++ medicalstore/medicalstore.ts
@@ -0,0 +1,279 @@
+var medicalstore={
+    medicaldetails: {
+        nameofmedicalstore: "",
+        medicalid: "ms0101"
+    },
+    "racks":[{
+        id:"rack1",
+        self_Details:[{
+            shelfName:"self1",
+           tablets:[{
+               id:"tablet1",
+               totalnooftablet: 100,
+               availabletablets: 100,
+               saletablet: 0,
+            },
+            {
+                id:"tablet2",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet3",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            }],
+        },{
+            shelfName:"self2",
+            tablets:[{
+                id:"tablet4",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+             },
+             {
+                 id:"tablet5", 
+                 totalnooftablet: 100,
+                 availabletablets: 100,
+                 saletablet: 0,
+             },
+             {
+                 id:"tablet6",
+                 totalnooftablet: 100,
+                 availabletablets: 100,
+                 saletablet: 0,
+             }],
+         },{
+            shelfName:"self3",
+            tablets:[{
+                id:"tablet7",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+             },
+             {
+                 id:"tablet8", 
+                 totalnooftablet: 100,
+                 availabletablets: 100,
+                 saletablet: 0,
+             },
+             {
+                 id:"tablet9",
+                 totalnooftablet: 100,
+                 availabletablets: 100,
+                 saletablet: 0,
+             }],
+         }],
+    },{
+        id:"rack2",
+        self_Details:[{
+            shelfName:"self1",
+           tablets:[{
+               id:"tablet10",
+                totalnooftablet: 100,
+               availabletablets: 100,
+               saletablet: 0,
+            },
+            {
+               id:"tablet11",
+               totalnooftablet: 100,
+               availabletablets: 100,
+               saletablet: 0,
+            },
+            {
+                id:"tablet12",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            }
+            ],
+        },{
+            shelfName:"self2",
+            tablets:[{
+                id:"tablet13", 
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+             },
+             {
+                id:"tablet14",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+             },
+             {
+                 id:"tablet15",
+                 totalnooftablet: 100,
+                 availabletablets: 100,
+                 saletablet: 0,
+             }
+             ],
+         },{
+            shelfName:"self3",
+            tablets:[{
+                    id:"tablet16",
+                    totalnooftablet: 100,
+                    availabletablets: 100,
+                    saletablet: 0,
+                },
+                {
+                    id:"tablet17", 
+                    totalnooftablet: 100,
+                    availabletablets: 100,
+                    saletablet: 0,
+                },
+                {
+                id:"tablet18",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            }],
+             
+        }],
+    },{
+        id:"rack3",
+        self_Details:[{
+            shelfName:"self1",
+            tablets:[{
+                id:"tablet19", 
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet20",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet21", 
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            }],
+        },{
+            shelfName:"self2",
+            tablets:[{
+                id:"tablet22",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet23",
+                 totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet24",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            }],
+        },{
+            shelfName:"self3",
+            tablets:[{
+                id:"tablet25",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+
+            },
+            {
+                id:"tablet26",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet27", 
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            }],
+        }],
+    },{
+        id:"rack4",
+        self_Details:[{
+            shelfName:"self1",
+            tablets:[{
+                id:"tablet28",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet29",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet30", 
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+
+            }],
+        },{
+            shelfName:"self2",
+            tablets:[{
+                id:"tablet31", 
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet32",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet33",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            }],
+        },{
+            shelfName:"self3",
+            tablets:[{
+                id:"tablet34",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet35",
+                totalnooftablet: 100,
+                availabletablets: 100,
+                saletablet: 0,
+            },
+            {
+                id:"tablet36",
+                 totalnooftablet: 100,
+                    availabletablets: 100,
+                    saletablet: 0,
+            }],
+        }],
+    }],
+    
+};
+var a = "tablet10";
+var result=search(a,medicalstore);
+function search(a,rack_details){
+for (var i = 0; i <= rack_details.racks.length; i++) {
+    for (var j = 0; j < rack_details.racks[i]['self_Details'].length; j++) {
+        for (var k = 0; k < rack_details.racks[i]['self_Details'][j]['tablets'].length; k++) {
+            if (rack_details.racks[i]['self_Details'][j]['tablets'][k].id === a) {
+                return "racks" + (i + 1) +"..."+ "self" + (j + 1)+"..." + "tablets"+(k + 1)+ "--" +JSON.stringify(rack_details.racks[i]['self_Details'][j]['tablets'][k]);
+            }
+        }
+    }
+}
+};
+console.log(result);
